925abf99008afb8595f847b4fdbef5fbb650f39a
Danilo Lima <danilo.notsys@live.com>
2025-08-31 10:27:43 -0300

    Commit

--- Radar/RadarCustom_files/mapaRadar.js
+++ Radar/RadarCustom_files/mapaRadar.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     //var layerOSM = L.tileLayer('https://mt.google.com/vt/lyrs=r&f=q&x={x}&y={y}&z={z}&s=Gal&apistyle=s.t%3A3|s.e%3Ag|p.v%3Aoff,s.t%3A3|s.e%3Al|p.v%3Aoff,s.t%3A2|s.e%3Al|p.v%3Aoff', {
     var layerOSM = L.tileLayer('https://mt.google.com/vt/lyrs=r,traffic&f=q&x={x}&y={y}&z={z}&s=Gal&apistyle=s.t%3A3|s.e%3Al|p.v%3Aoff,s.t%3A2|s.e%3Al|p.v%3Aoff', {
         attribution: '',
-        minZoom: 10,
+        minZoom: 8,
         maxZoom: 16,
     });
 
-    let defaultZoom = 7;
+    let defaultZoom = 8;
 
     if (window.matchMedia("(min-width:1024px)").matches) {
-        defaultZoom = 9;
+        defaultZoom = 10;
     }
 
     var mymap = L.map('mapid',
